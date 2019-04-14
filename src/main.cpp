3bfd3ccaf8d1c995e32392ca5cbff14867487fd4
RafaAudibert <rafaeelaudibert@gmail.com>
2019-04-13 22:53:15 -0300

    :sparkles::rocket: Added default code for the main
    Added some basic movimentation code

--- src/main.cpp
+++ src/main.cpp
@@ -15,26 +15,39 @@
 #include <GLFW/glfw3.h> // Criação de janelas do sistema operacional
 
 // Headers locais, definidos na pasta "include/"
-#include "engine/game.hpp"
-#include "engine/resource_manager.hpp"
+#include "engine\resource_manager.hpp"
+#include "engine\matrices.hpp"
+#include "engine\camera.hpp"
+#include "engine\game.hpp"
 #include "utils.h"
 
 // Activate NVIDIA off-board GPU, only if needed
- extern "C" {
-      _declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
- }
+extern "C"
+{
+    _declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+}
 
 // GLFW function declerations
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 
 // Helper functions which should be moved
-void print_info();
+void print_gpu_info();
 
 // The Width of the screen
 const GLuint SCREEN_WIDTH = 800;
 // The height of the screen
 const GLuint SCREEN_HEIGHT = 600;
+
+// DeltaTime variables
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+
+float lastX = SCREEN_WIDTH / 2.0f;
+float lastY = SCREEN_HEIGHT / 2.0f;
+bool firstMouse = true;
 
 Game TowerDefender = Game(SCREEN_WIDTH, SCREEN_HEIGHT);
 
@@ -56,9 +69,9 @@ int main(int argc, char *argv[])
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    #ifdef __APPLE__
+#ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    #endif
+#endif
 
     // Pedimos para utilizar o perfil "core", isto é, utilizaremos somente as
     // funções modernas de OpenGL.
@@ -66,8 +79,8 @@ int main(int argc, char *argv[])
 
     // Criamos uma janela do sistema operacional, com 800 colunas e 800 linhas
     // de pixels, e com título "INF01047 ...".
-    GLFWwindow* window;
-    window = glfwCreateWindow(800, 800, "INF01047 - 00287695 - Rafael Baldasso Audibert", NULL, NULL);
+    GLFWwindow *window;
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "INF01047 - 00287695 - Rafael Baldasso Audibert", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -81,32 +94,110 @@ int main(int argc, char *argv[])
     // ... ou clicar os botões do mouse ...
     // glfwSetMouseButtonCallback(window, MouseButtonCallback);
     // ... ou movimentar o cursor do mouse em cima da janela ...
-    // glfwSetCursorPosCallback(window, CursorPosCallback);
+    glfwSetCursorPosCallback(window, mouse_callback);
     // ... ou rolar a "rodinha" do mouse.
-    // glfwSetScrollCallback(window, ScrollCallback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // Definimos a função de callback que será chamada sempre que a janela for
     // redimensionada, por consequência alterando o tamanho do "framebuffer"
     // (região de memória onde são armazenados os pixels da imagem).
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetWindowSize(window, 800, 800); // Forçamos a chamada do callback acima, para definir g_ScreenRatio.
+    glfwSetWindowSize(window, SCREEN_WIDTH, SCREEN_HEIGHT); // Forçamos a chamada do callback acima, para definir g_ScreenRatio.
 
     // Indicamos que as chamadas OpenGL deverão renderizar nesta janela
     glfwMakeContextCurrent(window);
 
     // Carregamento de todas funções definidas por OpenGL 3.3, utilizando a
     // biblioteca GLAD.
-    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        glfwTerminate();
+        fprintf(stderr, "ERROR: Failed to initialize GLAD.\n");
+        std::exit(EXIT_FAILURE);
+    }
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
 
     // Imprimimos no terminal informações sobre a GPU do sistema
-    print_info();
+    print_gpu_info();
+
+    // Initialize shaders
+    Shader shader = ResourceManager::load_shader("../../src/camera.vs", "../../src/camera.fs", nullptr, "camera");
+
+    GLfloat vertices[] = {
+        //    X      Y     Z     W
+        -0.5f, 0.5f, 0.5f, 1.0f,   // posição do vértice 0
+        -0.5f, -0.5f, 0.5f, 1.0f,  // posição do vértice 1
+        0.5f, -0.5f, 0.5f, 1.0f,   // posição do vértice 2
+        0.5f, 0.5f, 0.5f, 1.0f,    // posição do vértice 3
+        -0.5f, 0.5f, -0.5f, 1.0f,  // posição do vértice 4
+        -0.5f, -0.5f, -0.5f, 1.0f, // posição do vértice 5
+        0.5f, -0.5f, -0.5f, 1.0f,  // posição do vértice 6
+        0.5f, 0.5f, -0.5f, 1.0f,   // posição do vértice 7
+    };
+
+    // world space positions of our cubes
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f)};
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    GLuint indices[] = {
+        // Definimos os índices dos vértices que definem as FACES de um cubo
+        // através de 12 triângulos que serão desenhados com o modo de renderização
+        // GL_TRIANGLES.
+        0, 1, 2, // triângulo 1
+        7, 6, 5, // triângulo 2
+        3, 2, 6, // triângulo 3
+        4, 0, 3, // triângulo 4
+        4, 5, 1, // triângulo 5
+        1, 5, 6, // triângulo 6
+        0, 2, 3, // triângulo 7
+        7, 5, 4, // triângulo 8
+        3, 6, 7, // triângulo 9
+        4, 3, 7, // triângulo 10
+        4, 1, 0, // triângulo 11
+        1, 6, 2, // triângulo 12
+    };
+
+    // Criamos um buffer OpenGL para armazenar os índices acima
+    GLuint indices_id;
+    glGenBuffers(1, &indices_id);
+
+    // "Ligamos" o buffer. Note que o tipo agora é GL_ELEMENT_ARRAY_BUFFER.
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_id);
+
+    // Alocamos memória para o buffer, e copiamos ele
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    shader.use();
 
     // Initialize game
     TowerDefender.init();
-
-    // DeltaTime variables
-    GLfloat deltaTime = 0.0f;
-    GLfloat lastFrame = 0.0f;
 
     // Start Game within Menu State
     TowerDefender.state = GAME_ACTIVE;
@@ -126,13 +217,33 @@ int main(int argc, char *argv[])
         TowerDefender.update(deltaTime);
 
         // Render
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         TowerDefender.render();
 
+        glm::mat4 projection = glm::perspective(glm::radians(TowerDefender.camera.zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+        shader.set_matrix("projection", projection);
+
+        // camera/view transformation
+        glm::mat4 view = TowerDefender.camera.get_view_matrix();
+
+        shader.set_matrix("view", view);
+
+        // render boxes
+        glBindVertexArray(VAO);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            shader.set_matrix("model", model);
+
+            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        }
+
         glfwSwapBuffers(window);
-
-
     }
 
     // Delete all resources as loaded using the resource manager
@@ -160,7 +271,7 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 // Definição da função que será chamada sempre que a janela do sistema
 // operacional for redimensionada, por consequência alterando o tamanho do
 // "framebuffer" (região de memória onde são armazenados os pixels da imagem).
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     // Indicamos que queremos renderizar em toda região do framebuffer. A
     // função "glViewport" define o mapeamento das "normalized device
@@ -180,11 +291,39 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     TowerDefender.screen_ratio = (float)width / height;
 }
 
-void print_info(){
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow *window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
 
-    const GLubyte *vendor      = glGetString(GL_VENDOR);
-    const GLubyte *renderer    = glGetString(GL_RENDERER);
-    const GLubyte *glversion   = glGetString(GL_VERSION);
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    TowerDefender.camera.process_mouse_movement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
+{
+    TowerDefender.camera.process_mouse_scroll(yoffset);
+}
+
+void print_gpu_info()
+{
+
+    const GLubyte *vendor = glGetString(GL_VENDOR);
+    const GLubyte *renderer = glGetString(GL_RENDERER);
+    const GLubyte *glversion = glGetString(GL_VERSION);
     const GLubyte *glslversion = glGetString(GL_SHADING_LANGUAGE_VERSION);
 
     printf("GPU: %s, %s, OpenGL %s, GLSL %s\n", vendor, renderer, glversion, glslversion);
