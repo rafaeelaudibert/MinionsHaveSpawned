0a258d791cf7b6a868143f4d56269dd2d72f8414
RafaAudibert <rafaeelaudibert@gmail.com>
2019-06-14 23:58:01 -0300

    :recycle: Removed unnecessary comments

--- src/model/dummy.cpp
+++ src/model/dummy.cpp
@@ -4,23 +4,12 @@
 
 void Dummy::build()
 {
-
-    // poro.obj e poro.jpg
-    // elder_dragon.obj e sru_dragon_elder_tx_cm.png
-    // ha_chaosminion_melee.obj e chaosminion_melee_tx_cm.jpg
-    // ha_chaosminion_ranged.obj e chaosminion_ranged_tx_cm.jpg
-    // ha_chaosminion_siege.obj e chaosminion_siege_tx_cm.jpg
-    // ha_chaosminion_super.obj e chaosminion_super_tx_cm.jpg
-    // ha_chaosturret.obj e ha_ap_chaosturret_blue/red.jpg
-    // order_turret_1.obj e sruap_turret_order1_tx_cm.jpg
-    // zzrot_minion.obj e zzrot_minion_tx_cm.jpg
-
     // Initialize object, shaders, and textures
-    ResourceManager::load_object("../../src/objects/elder_dragon.obj", this, this->name);
+    ResourceManager::load_object("../../src/objects/dummy.obj", this, this->name);
     this->shader = ResourceManager::load_shader("../../src/shaders/default_texture.vs", "../../src/shaders/default_texture.fs", nullptr, this->name);
     this->shader.use();
 
-    this->texture = ResourceManager::load_texture("../../src/textures/sru_dragon_elder_tx_cm.jpg", this->name);
+    this->texture = ResourceManager::load_texture("../../src/textures/practicetool_targetdummy_blue_tx_cm.jpg", this->name);
 
     return;
 }
