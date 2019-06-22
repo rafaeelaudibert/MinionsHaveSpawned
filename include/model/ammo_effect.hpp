9675dc42b719665a54c34e7b81ccffa02e7d040c
RafaAudibert <rafaeelaudibert@gmail.com>
2019-06-22 20:35:39 -0300

    :sparkles: Partially dealing with the effects on Enemy::update

--- include/model/ammo_effect.hpp
+++ include/model/ammo_effect.hpp
@@ -6,11 +6,13 @@ namespace AmmoEffects
     class AmmoEffect
     {
     protected:
-        AmmoEffect(float ammo_power = 0.0f, float ammo_time = 0.0f) : ammo_power(ammo_power), ammo_time(ammo_time) {};
+        AmmoEffect(float ammo_damage = 0.0f, float ammo_time = 0.0f, float ammo_speed_multiplier = 1.0f) : ammo_damage(ammo_damage), ammo_time(ammo_time), ammo_speed_multiplier(ammo_speed_multiplier) {};
 
     public:
-        float ammo_power = 0.0f;
+        float ammo_damage = 0.0f;
         float ammo_time = 0.0f;
+        float ammo_speed_multiplier = 1.0f;
+        std::string ammo_type = "";
 
         virtual void update(float delta_time)
         {
@@ -22,32 +24,37 @@ namespace AmmoEffects
     class NoneEffect : public AmmoEffect
     {
     public:
-        NoneEffect() : AmmoEffect(0.0f, 0.0f) { }
+        NoneEffect() : AmmoEffect(0.0f, 0.0f, 1.0f) { }
+        std::string ammo_type = "none";
     };
 
 
     class AreaDamageEffect : public AmmoEffect
     {
     public:
-        AreaDamageEffect(float ammo_power = 7.5f, float ammo_time = 0.0f) : AmmoEffect(ammo_power, ammo_time) { };
+        AreaDamageEffect(float ammo_damage = 7.5f, float ammo_time = 0.0f, float ammo_speed_multiplier = 1.0f) : AmmoEffect(ammo_damage, ammo_time, ammo_speed_multiplier) { };
+        std::string ammo_type = "aoe";
     };
 
     class DamagePerSecondEffect : public AmmoEffect
     {
     public:
-        DamagePerSecondEffect(float ammo_power = 5.0f, float ammo_time = 2.0f) : AmmoEffect(ammo_power, ammo_time) { };
+        DamagePerSecondEffect(float ammo_damage = 5.0f, float ammo_time = 2.0f, float ammo_speed_multiplier = 1.0f) : AmmoEffect(ammo_damage, ammo_time, ammo_speed_multiplier) { };
+        std::string ammo_type = "dps";
     };
 
     class SlowEffect : public AmmoEffect
     {
     public:
-        SlowEffect(float ammo_power = 0.5f, float ammo_time = 2.0f) : AmmoEffect(ammo_power, ammo_time) { };
+        SlowEffect(float ammo_damage = 0.0f, float ammo_time = 2.0f, float ammo_speed_multiplier = 0.65f) : AmmoEffect(ammo_damage, ammo_time, ammo_speed_multiplier) { };
+        std::string ammo_type = "slow";
     };
 
     class FreezingEffect : public AmmoEffect
     {
     public:
-        FreezingEffect(float ammo_power = 1.0f, float ammo_time = 1.0f) : AmmoEffect(ammo_power, ammo_time) { };
+        FreezingEffect(float ammo_damage = 0.0f, float ammo_time = 0.75f, float ammo_speed_multiplier = 0.0f) : AmmoEffect(ammo_damage, ammo_time, ammo_speed_multiplier) { };
+        std::string ammo_type = "slow";
     };
 }
 
--- src/model/enemy.cpp
+++ src/model/enemy.cpp
@@ -1,23 +1,48 @@
 #include "model/enemy.hpp"
+#include <algorithm>
 
-void Enemy::update(float delta_time) {
-    for (AmmoEffects::AmmoEffect effect : this->effects) {
+void Enemy::update(float delta_time)
+{
+    for (AmmoEffects::AmmoEffect effect : this->effects)
+    {
         effect.update(delta_time);
     }
 
     // Remove the effects which are over
-    auto res = std::remove_if(this->effects.begin(), this->effects.end(), [](auto const x) {return x.ammo_time <= 0.0f});
+    auto res = std::remove_if(this->effects.begin(), this->effects.end(), [](AmmoEffects::AmmoEffect const x)
+    {
+        return x.ammo_time <= 0.0f;
+    });
     this->effects.erase(res, this->effects.end());
 
-    // TODO: Need to figure it out how to properly use the effects applied
+    float accumulated_damage = 0.0f;
+    float accumulated_speed = 1.0f;
+    for (const AmmoEffects::AmmoEffect effect : this->effects)
+    {
+        // Aoe effects are handled in a different way, splitting the damage to the enemies in a radius
+        if (effect.ammo_type != "aoe") {
+            accumulated_damage += effect.ammo_damage * delta_time;
+            accumulated_speed *= effect.ammo_speed_multiplier;
+        } else {
+            // TODO: For enemies in a certain radial distance from this enemy, apply effect->ammo_damage once
+        }
+    }
+
+    // Hit the damage accumulated in the computation above
+    this->hit(accumulated_damage);
+
+    // TODO: Walk multiplying by the speed computed above
 }
 
-bool Enemy::is_dead(){
+bool Enemy::is_dead()
+{
     return this->current_life_points < 0;
 }
 
-void Enemy::hit(float life_points, AmmoEffects::AmmoEffect *ammo_effect) {
-    if (!this->is_dead()) {
+void Enemy::hit(float life_points, AmmoEffects::AmmoEffect *ammo_effect)
+{
+    if (!this->is_dead())
+    {
         this->current_life_points -= life_points;
 
         if (ammo_effect != nullptr)
@@ -27,7 +52,8 @@ void Enemy::hit(float life_points, AmmoEffects::AmmoEffect *ammo_effect) {
     return;
 }
 
-void Enemy::render_health_bar(glm::mat4 view, glm::mat4 projection) {
+void Enemy::render_health_bar(glm::mat4 view, glm::mat4 projection)
+{
 
     if (!this->is_dead())
         this->health_bar->render(view, projection, this->current_life_points / this->max_life_points);
